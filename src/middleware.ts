ec05c009dc87001270cde0b68981d7703ca78626
Robert McDowell <doodersrage@gmail.com>
2026-06-01 16:28:39 -0400

    page routing middleware fix

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  // If the user visits /blog, let it reach your specific blog file
+  if (request.nextUrl.pathname.startsWith('/blog')) {
+    return NextResponse.next()
+  }
+
+  // Rewrite everything else to the catch-all page
+  return NextResponse.rewrite(new URL(`${request.nextUrl.pathname}`, request.url))
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+}
